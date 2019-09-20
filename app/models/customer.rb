ff04239bbb34ce2581ba85b2049c27bfa865cbdf
EDownie <53910027+ED1978@users.noreply.github.com>
2019-09-20 18:24:57 +0100

    Customer class setup with save functionality

---
+++ app/models/customer.rb
@@ -0,0 +1,19 @@
+class Customer
+
+  attr_reader :id, :name
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+  end
+
+  def save()
+    sql = 'INSERT INTO customers (name)
+    VALUES ($1)
+    RETURNING id'
+    values = [@name]
+    results = SqlRunner.run(sql,values)
+    @id = results.first()['id'].to_i()
+  end
+
+end
