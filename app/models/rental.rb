d7b4557446fa2a28de645eda39d020827eb54015
EDownie <53910027+ED1978@users.noreply.github.com>
2019-09-20 18:47:02 +0100

    Rental class creted with save method

---
+++ app/models/rental.rb
@@ -0,0 +1,20 @@
+class Rental
+
+  attr_reader :id, :customer_id, :stock_item_id
+
+  def initialize(options)
+    @id = options['id'].to_i() if options['id']
+    @customer_id = options['customer_id'].to_i()
+    @stock_item_id = options['stock_item_id'].to_i()
+  end
+
+  def save()
+    sql = "INSERT INTO rentals (customer_id, stock_item_id)
+    VALUES ($1, $2)
+    RETURNING id"
+    values = [@customer_id, @stock_item_id]
+    results = SqlRunner.run(sql, values)
+    @id = results.first['id'].to_i()
+  end
+
+end
