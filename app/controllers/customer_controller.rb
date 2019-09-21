20f2b4a41c30dfbfce44d94b1303fc47ee53fffd
EDownie <53910027+ED1978@users.noreply.github.com>
2019-09-21 13:19:07 +0100

    add customer

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -8,3 +8,13 @@ get '/customers' do
   @customers = Customer.all
   erb(:"customers/index")
 end
+
+get '/customers/new' do
+  erb(:"customers/new")
+end
+
+post '/customers' do
+  customer = Customer.new(params)
+  customer.save()
+  erb(:"customers/add_success")
+end
