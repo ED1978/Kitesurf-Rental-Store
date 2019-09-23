397fc7333e9f0fe6547e8370ebf11c0c8bb5da14
EDownie <53910027+ED1978@users.noreply.github.com>
2019-09-23 16:42:30 +0100

    customers showing added attributes in index

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,7 @@ require_relative('../db/sql_runner.rb')
 
 class Customer
 
-  attr_reader :id, :name
+  attr_reader :id, :name, :address, :dob
 
   def initialize(options)
     @id = options['id'].to_i if options['id']
